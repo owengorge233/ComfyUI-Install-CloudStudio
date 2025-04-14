a8027b1bc685514b552b018f935bf475c7f0cf89
peter.zhao <tinypeter@163.com>
2025-04-14 19:52:36 +0800

    modify env.sh install.sh

--- env.sh
+++ env.sh
@@ -1,31 +1,31 @@
 #! /bin/bash
-#echo 'export http_proxy=http://proxy.cloudstudio.work:8081' >> ~/.bashrc
-#echo 'export HTTP_PROXY=http://proxy.cloudstudio.work:8081' >> ~/.bashrc
-#echo 'export https_proxy=http://proxy.cloudstudio.work:8081' >> ~/.bashrc
-#echo 'export HTTPS_PROXY=http://proxy.cloudstudio.work:8081' >> ~/.bashrc
-#echo 'export no_proxy=127.0.0.1,localhost,.local,.tencent.com,tencentyun.com,ppa.launchpad.net,0.0.0.0' >> ~/.bashrc
-#echo 'export NO_PROXY=127.0.0.1,localhost,.local,.tencent.com,tencentyun.com,ppa.launchpad.net,0.0.0.0' >> ~/.bashrc
+echo 'export http_proxy=http://proxy.cloudstudio.work:8081' >> ~/.bashrc
+echo 'export HTTP_PROXY=http://proxy.cloudstudio.work:8081' >> ~/.bashrc
+echo 'export https_proxy=http://proxy.cloudstudio.work:8081' >> ~/.bashrc
+echo 'export HTTPS_PROXY=http://proxy.cloudstudio.work:8081' >> ~/.bashrc
+echo 'export no_proxy=127.0.0.1,localhost,.local,.tencent.com,tencentyun.com,ppa.launchpad.net,0.0.0.0' >> ~/.bashrc
+echo 'export NO_PROXY=127.0.0.1,localhost,.local,.tencent.com,tencentyun.com,ppa.launchpad.net,0.0.0.0' >> ~/.bashrc
 
 # （可选）保存到 zshrc
-#echo 'export http_proxy=http://proxy.cloudstudio.work:8081' >> ~/.zshrc
-#echo 'export HTTP_PROXY=http://proxy.cloudstudio.work:8081' >> ~/.zshrc
-#echo 'export https_proxy=http://proxy.cloudstudio.work:8081' >> ~/.zshrc
-#echo 'export HTTPS_PROXY=http://proxy.cloudstudio.work:8081' >> ~/.zshrc
-#echo 'export no_proxy=127.0.0.1,localhost,.local,.tencent.com,tencentyun.com,ppa.launchpad.net,0.0.0.0' >> ~/.zshrc
-#echo 'export NO_PROXY=127.0.0.1,localhost,.local,.tencent.com,tencentyun.com,ppa.launchpad.net,0.0.0.0' >> ~/.zshrc
-#echo 'export HF_ENDPOINT=https://hf-mirror.com' >> ~/.bashrc   
+echo 'export http_proxy=http://proxy.cloudstudio.work:8081' >> ~/.zshrc
+echo 'export HTTP_PROXY=http://proxy.cloudstudio.work:8081' >> ~/.zshrc
+echo 'export https_proxy=http://proxy.cloudstudio.work:8081' >> ~/.zshrc
+echo 'export HTTPS_PROXY=http://proxy.cloudstudio.work:8081' >> ~/.zshrc
+echo 'export no_proxy=127.0.0.1,localhost,.local,.tencent.com,tencentyun.com,ppa.launchpad.net,0.0.0.0' >> ~/.zshrc
+echo 'export NO_PROXY=127.0.0.1,localhost,.local,.tencent.com,tencentyun.com,ppa.launchpad.net,0.0.0.0' >> ~/.zshrc
+echo 'export HF_ENDPOINT=https://hf-mirror.com' >> ~/.bashrc   
 
-#source ~/.bashrc
+source ~/.bashrc
 
-#pip install -U huggingface_hub
+pip install -U huggingface_hub
 
 # Git 设置全局代理
-#git config --global http.proxy http://proxy.cloudstudio.work:8081
-#git config --global https.proxy http://proxy.cloudstudio.work:8081
+git config --global http.proxy http://proxy.cloudstudio.work:8081
+git config --global https.proxy http://proxy.cloudstudio.work:8081
 
 # 修复 apt update 失败问题
-#cp /etc/apt/sources.list /etc/apt/sources.list.bak.`date +'%F-%T'`
-#sed -i 's/http:\/\/mirrors.cloud.tencent.com/https:\/\/mirrors.cloud.tencent.com/g' /etc/apt/sources.list
+cp /etc/apt/sources.list /etc/apt/sources.list.bak.`date +'%F-%T'`
+sed -i 's/http:\/\/mirrors.cloud.tencent.com/https:\/\/mirrors.cloud.tencent.com/g' /etc/apt/sources.list
 
 # 支持通过 ssh 方式 clone github 仓库
 apt update -y && apt install -y socat
