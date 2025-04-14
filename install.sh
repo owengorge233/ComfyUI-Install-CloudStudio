a4de2028d439f8dab6f595777479a2295ca6eda2
peter.zhao <tinypeter@163.com>
2025-04-14 13:27:42 +0800

    modify env.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,10 @@ pip install --no-cache-dir -r "$basefolder/ComfyUI/requirements.txt"
 check_exit $? "ComfyUI依赖安装失败"
 
 echo "▂▂▂▂▂▂▂▂▂▂ 安装PyTorch ▂▂▂▂▂▂▂▂▂▂"
-pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
+pip install --pre torch torchvision torchecho "▂▂▂▂▂▂▂▂▂▂ 安装Flux ▂▂▂▂▂▂▂▂▂▂"
+cd "$basefolder/ComfyUI/models/unet" || exit
+
+audio --index-url https://download.pytorch.org/whl/nightly/cu128
 check_exit $? "PyTorch安装失败"
 
 echo "▂▂▂▂▂▂▂▂▂▂ 安装ComfyUI管理器 ▂▂▂▂▂▂▂▂▂▂"
@@ -43,5 +46,30 @@ git clone --progress https://github.com/city96/ComfyUI-GGUF comfyui-gguf
 check_exit $? "ComfyUI GGUF克隆失败"
 pip install --no-cache-dir -r "$basefolder/ComfyUI/custom_nodes/comfyui-gguf/requirements.txt"
 check_exit $? "ComfyUI GGUF依赖安装失败"
+
+echo "▂▂▂▂▂▂▂▂▂▂ 安装Flux▂▂▂▂▂▂▂▂▂▂"
+cd "$basefolder/ComfyUI/models/unet" || exit
+wget -O flux1-schnell-Q4_K_S.gguf https://huggingface.co/city96/FLUX.1-schnell-gguf/resolve/main/flux1-schnell-Q4_K_S.gguf?download=true
+check_exit $? "安装Flux失败"
+
+echo "▂▂▂▂▂▂▂▂▂▂ 安装Flux-schnell ▂▂▂▂▂▂▂▂▂▂"
+cd "$basefolder/ComfyUI/models/unet" || exit
+wget -O flux1-schnell-Q4_K_S.gguf https://huggingface.co/city96/FLUX.1-schnell-gguf/resolve/main/flux1-schnell-Q4_K_S.gguf?download=true
+check_exit $? "安装Flux-schnell失败"
+wget -O t5-v1_1-xxl-encoder-Q4_K_S.gguf https://huggingface.co/city96/t5-v1_1-xxl-encoder-gguf/blob/main/t5-v1_1-xxl-encoder-Q4_K_S.gguf?download=true
+check_exit $? "安装t5-v1_1-xxl-encoder-Q4_K_S.gguf失败"
+
+echo "▂▂▂▂▂▂▂▂▂▂ 安装Clip ▂▂▂▂▂▂▂▂▂▂"
+cd "$basefolder/ComfyUI/models/clip" || exit
+wget -O clip_l.safetensors https://www.modelscope.cn/models/livehouse/clip_l/resolve/master/clip_l.safetensors
+check_exit $? "安装clip_l.safetensors失败"
+wget -O t5xxl_fp8_e4m3fn.safetensors https://www.modelscope.cn/models/zc0501/t5xxl_fp8/resolve/20250322200338/t5xxl_fp8_e4m3fn.safetensors
+check_exit $? "安装t5xxl_fp8_e4m3fn.safetensors失败"
+
+echo "▂▂▂▂▂▂▂▂▂▂ 安装vae ▂▂▂▂▂▂▂▂▂▂"
+cd "$basefolder/ComfyUI/models/vae" || exit
+wget -O ae.safetensors https://huggingface.co/black-forest-labs/FLUX.1-schnell/blob/main/ae.safetensors?download=true
+check_exit $? "ae.safetensors失败"
+
 
 echo "✅ 所有组件安装完成"
